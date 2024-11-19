6e0466266adaf9fe851aea753f46bfa7a08ce536
Erik Jacome <138533562+erikenrique@users.noreply.github.com>
2024-11-19 14:31:45 -0500

    initial commit personal express

--- public/main.js
+++ public/main.js
@@ -1,45 +1,86 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+let upvoteButtons = document.getElementsByClassName("fa-thumbs-up");
+let downvoteButtons = document.getElementsByClassName("fa-thumbs-down");
+let deleteButtons = document.getElementsByClassName("fa-trash-o");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+Array.from(upvoteButtons).forEach(function(element) {
+  element.addEventListener('click', function(){
+    // console.log(this.parentNode.parentNode)
+    const topic = this.parentNode.parentNode.childNodes[3].innerText
+    const description = this.parentNode.parentNode.childNodes[7].innerText
+    const upvotes = parseFloat(this.parentNode.parentNode.childNodes[15].innerText)
+    fetch('addUpvote', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'topic': topic,
+        'description': description,
+        'upvotes': upvotes
+      })
+    })
+    .then(response => response.ok ? response.json() : null)
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+Array.from(downvoteButtons).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const topic = this.parentNode.parentNode.childNodes[3].innerText
+    const description = this.parentNode.parentNode.childNodes[7].innerText
+    const upvotes = parseFloat(this.parentNode.parentNode.childNodes[15].innerText)
+    fetch('addDownvote', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'topic': topic,
+        'description': description,
+        'upvotes': upvotes
+      })
+    })
+    .then(response => response.ok ? response.json() : null)
+    .then(data => window.location.reload(true))
+  });
+});
+
+Array.from(deleteButtons).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const topic = this.parentNode.parentNode.childNodes[3].innerText
+    const description = this.parentNode.parentNode.childNodes[7].innerText
+    fetch('topics', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'topic': topic,
+        'description': description
+      })
+    }).then(() => {
+      window.location.reload()
+    }
+  )
+  });
+});
+
+
+function showPopup(imageSrc) {
+  document.getElementById("popupImage").src = imageSrc;
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".overlay").addEventListener("click", closePopup);
+  document.querySelectorAll('.thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+      showPopup(thumbnail.src)
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,48 +1,86 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+// set up ======================================================================
+// get all the tools we need
 
-var db, collection;
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'study_tracker';
 
-app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`);
         db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
-});
+        
+        // Start the server only after DB connection is successful
+        app.listen(process.env.PORT || 3000, () => {
+          console.log(`Server running on port ${process.env.PORT || 3000}`);
+        });
+    })
+    .catch(error => console.error("Failed to connect to the database:", error));
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(fileUpload());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
-  })
-})
+  if (!db) {
+      return res.status(500).send("Database connection not established");
+  }
+  db.collection('topics').find().sort({ upvotes: -1 }).toArray((err, result) => {
+      if (err) return console.log(err);
+      res.render('index.ejs', { topics: result });
+  });
+});
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+app.post('/topics', (req, res) => {
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+  const bodyData = Object.assign({}, req.body);
+  console.log('Form data:', bodyData);
+  console.log('File data:', req.files);
+
+
+  // convert image to binary data for mongoDB upload
+  const imageBuffer = req.files && req.files.image ? req.files.image.data : null;
+
+  db.collection('topics').insertOne({
+    topic: bodyData.topic,
+    description: bodyData.description,
+    level: bodyData.level,
+    upvotes: 0,
+    image: imageBuffer // save the image data in mongodb
+  }, (err, result) => {
+    if (err) return console.log(err, req);
+    console.log('Topic saved to database');
+    res.redirect('/');
+  });
+});
+
+
+
+app.put('/addUpvote', (req, res) => {
+  db.collection('topics').findOneAndUpdate(
+    { topic: req.body.topic, description: req.body.description },
+    { $set: { upvotes: req.body.upvotes + 1 } },
+    { sort: { _id: -1 }, upsert: true },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+  );
+});
+
+app.put('/addDownvote', (req, res) => {
+  db.collection('topics')
+  .findOneAndUpdate({topic: req.body.topic, description: req.body.description}, {
     $set: {
-      thumbUp:req.body.thumbUp + 1
+      upvotes: req.body.upvotes - 1
     }
   }, {
     sort: {_id: -1},
@@ -53,9 +91,14 @@ app.put('/messages', (req, res) => {
   })
 })
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+app.delete('/topics', (req, res) => {
+  db.collection('topics').findOneAndDelete({topic: req.body.topic, description: req.body.description}, (err, result) => {
     if (err) return res.send(500, err)
-    res.send('Message deleted!')
+    res.send('Topic deleted!')
   })
 })
+
+// launch ======================================================================
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`)
+// })
